521969781663d655ea6f9d659c8d1dd77ba4246c
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-23 18:24:41 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,44 @@
+class Board
+
+  attr_accessor :cells
+
+  def initialize
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def reset!
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(input)
+    @cells[input.to_i-1]
+  end
+
+  def full?
+    @cells.all? {|cell| cell == "O" || cell == "X"}
+  end
+
+  def turn_count
+    @cells.select{|cell| cell == "O" || cell == "X"}.size
+  end
+
+  def taken?(input)
+    position(input) == "X" || position(input) == "O"
+  end
+
+  def valid_move?(input)
+    input.to_i.between?(1,9) && !taken?(input)
+  end
+
+  def update(cell, player)
+    @cells[cell.to_i-1] = player.token
+  end
+end
